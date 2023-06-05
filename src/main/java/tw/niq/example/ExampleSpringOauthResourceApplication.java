4548e54034beaa22f4ea69a0aab499927edd3b01
Nick <nick.chen@cmp.com.tw>
2023-06-05 10:52:27 +0800

    Initial commit

---
+++ src/main/java/tw/niq/example/ExampleSpringOauthResourceApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringOauthResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringOauthResourceApplication.class, args);
+	}
+
+}

1911c9415363bf68b55171698ae34b2c66b061a5
Nick <nick.chen@cmp.com.tw>
2023-07-26 15:43:10 +0800

    Add role security

--- src/main/java/tw/niq/example/config/WebSecurityConfig.java
+++ src/main/java/tw/niq/example/config/WebSecurityConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import tw.niq.example.security.KeyclockRoleConverter;
 
 @Configuration
 @EnableWebSecurity
@@ -14,12 +17,20 @@ public class WebSecurityConfig {
 	@Bean
 	SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyclockRoleConverter());
+		
 		http
 			.authorizeHttpRequests(authorizeHttpRequests -> 
 				authorizeHttpRequests
-					.requestMatchers(HttpMethod.GET, "/users/status/check").hasAuthority("SCOPE_profile")
+					.requestMatchers(HttpMethod.GET, "/users/status/check")
+//						.hasAuthority("SCOPE_profile")
+//						.hasRole("developer")
+						.hasAnyAuthority("ROLE_developer")
 					.anyRequest().authenticated())
-			.oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(jwt -> {}));
+			.oauth2ResourceServer(oauth2ResourceServer -> 
+				oauth2ResourceServer
+					.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
 		
 		return http.build();
 	}

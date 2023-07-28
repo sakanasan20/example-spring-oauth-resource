a052eceace914c58ec0dccd5ede2c7434f8d4bcb
Nick <nick.chen@cmp.com.tw>
2023-07-28 12:01:01 +0800

    Add cors config

--- src/main/java/tw/niq/example/config/WebSecurityConfig.java
+++ src/main/java/tw/niq/example/config/WebSecurityConfig.java
@@ -1,13 +1,19 @@
 package tw.niq.example.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import tw.niq.example.security.KeyclockRoleConverter;
 
@@ -15,6 +21,22 @@ import tw.niq.example.security.KeyclockRoleConverter;
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurityConfig {
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("*"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+		
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		
+		return urlBasedCorsConfigurationSource;
+	}
 
 	@Bean
 	SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -23,6 +45,8 @@ public class WebSecurityConfig {
 		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyclockRoleConverter());
 		
 		http
+			// CORS should configured either on gateway or resource server 
+//			.cors(Customizer.withDefaults())
 			.authorizeHttpRequests(authorizeHttpRequests -> 
 				authorizeHttpRequests
 					.requestMatchers(HttpMethod.GET, "/users/status/check")
